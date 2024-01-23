638e413be61ada71c15d2d90327a573c26a27972
Oriane Kopp <okopp@login8.hpc.binf.unibe.ch>
2024-01-23 14:38:46 +0100

    beautiful R code

--- exploratorydataanalysis.R
+++ exploratorydataanalysis.R
@@ -1,20 +1,14 @@
-##########################################################################################################
-#---------------------------------------------------------------------------------------------------------
-##########################################################################################################
-#Exploratory Data Analysis
-
 #set directory
 setwd(dir = "C:/Users/Orian/OneDrive/Documents/Master")
 
 #install packages
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-BiocManager::install('EnhancedVolcano')
 BiocManager::install("DESeq2")
 BiocManager::install("biomaRt")
-BiocManager::install("clusterProfiler")
 BiocManager::install("org.Mm.eg.db")
-
+BiocManager::install('EnhancedVolcano')
+BiocManager::install("clusterProfiler")
 
 #load library
 library(DESeq2)
@@ -24,9 +18,14 @@ library(EnhancedVolcano)
 library(clusterProfiler)
 
 
+#############################
+# Exploratory Data Analysis #------------------------------------------------------------------------------------------------------------
+#############################
+
 #import the count table and the column table (with information about the samples)
 count_table <- read.table("final_count_table.txt", header = T, row.names = 1, sep = "\t")
 col_table <- read.table("data_table.txt", header = T, row.names = 1, sep = "\t")
+
 
 #create the DESeqDataSet object
 dds <- DESeqDataSetFromMatrix(countData = count_table, colData = col_table, design = ~ Group)
@@ -34,18 +33,19 @@ dds <- DESeqDataSetFromMatrix(countData = count_table, colData = col_table, desi
 #perform the DE
 dds <- DESeq(dds)
 
+
 #PCA to vizualise the data
 vsd <- vst(dds, blind = TRUE) #to remove the variance on the mean 
-plotPCA(vsd, intgroup=c("Group"))
+plotPCA(vsd, intgroup = c("Group")) + ggtitle("Principal Component Analysis") + theme(plot.title = element_text(hjust = 0.5))
 
-##########################################################################################################
-#---------------------------------------------------------------------------------------------------------
-##########################################################################################################
-#Differential Expression Analysis
 
-###############################
+####################################
+# Differential Expression Analysis #-----------------------------------------------------------------------------------------------------
+####################################
+
+# # # # # # # # # # # # # # # #
 # Blood Case vs Blood Control #
-###############################
+# # # # # # # # # # # # # # # #
 
 results_blood <- results(dds, contrast=c("Group","Blood_WT_Case","Blood_WT_Control"))
 DE_blood <- results_blood[which(results_blood$padj < 0.05),]
@@ -62,6 +62,7 @@ gene_info_b <- getBM(attributes = c("ensembl_gene_id", "external_gene_name"),
 
 DE_blood_with_gene_names <- cbind(gene_info_b, DE_blood)
 
+
 #sort DE genes by pvalue to get the ten most DE genes
 sort_blood <- DE_blood_with_gene_names[order(DE_blood_with_gene_names$padj),]
 sort_blood[1:10,]
@@ -75,6 +76,7 @@ EnhancedVolcano(DE_blood_with_gene_names,
                 col = c("pink3", "olivedrab", "cyan3", "coral1"),
                 title = "DE genes - Blood Case vs Blood Control")
 
+
 #explore expression of genes from the article
 Rsad2 <-DE_blood["ENSMUSG00000020641",]
 Rsad2$log2FoldChange > 0 #False --> underexpression
@@ -86,13 +88,15 @@ oas1a$log2FoldChange > 0 #true -- >overexpression
 oas1a <- plotCounts(dds, "ENSMUSG00000052776", intgroup = c("Group"), returnData = TRUE)
 boxplot(count ~ Group , data=oas1a, main = "Expression of oas1a")
 
-#############################
+
+# # # # # # # # # # # # # # #
 # Lung Case vs Lung Control #
-#############################
+# # # # # # # # # # # # # # #
 
 results_lung <- results(dds, contrast=c("Group","Lung_WT_Case","Lung_WT_Control"))
 DE_lung <- results_lung[which(results_lung$padj < 0.05),]
 summary(DE_lung)
+
 
 #add genes name
 ensembl_gene_ids_l <- rownames(DE_lung)
@@ -103,9 +107,10 @@ gene_info_l <- getBM(attributes = c("ensembl_gene_id", "external_gene_name"),
 
 DE_lung_with_gene_names <- cbind(gene_info_l, DE_lung)
 
+
+#sort DE genes by pvalue to get the ten most DE genes
 sort_lung <- DE_lung_with_gene_names[order(DE_lung_with_gene_names$padj),]
 sort_lung[1:10,]
-
 
 
 #volcano plot
@@ -115,6 +120,7 @@ EnhancedVolcano(DE_lung_with_gene_names,
                 y = 'pvalue',
                 col = c("pink3", "olivedrab", "cyan3", "coral1"),
                 title = "DE genes -  Lung Case vs Lung Control")
+
 
 #explore expression of genes from the article
 irf7 <-DE_lung["ENSMUSG00000025498",]
@@ -127,42 +133,45 @@ fcgr1$log2FoldChange > 0 #True -- >overexpression
 fcgr1 <- plotCounts(dds, "ENSMUSG00000015947", intgroup = c("Group"), returnData = TRUE)
 boxplot(count ~ Group , data=fcgr1, main = "Expression of fcgr1")
 
-##########################################################################################################
-#---------------------------------------------------------------------------------------------------------
-##########################################################################################################
-#Overexpression Analysis 
 
-###############################
+###########################
+# Overexpression Analysis #--------------------------------------------------------------------------------------------------------------
+###########################
+
+# # # # # # # # # # # # # # # #
 # Blood Case vs Blood Control #
-###############################
+# # # # # # # # # # # # # # # #
 
 go_blood <- enrichGO(gene = row.names(DE_blood_with_gene_names), universe = names(dds), 
                      OrgDb = org.Mm.eg.db, ont= "BP", keyType = "ENSEMBL")
 
 head(go_blood)
 
+
 #barplot: count by go terms, sorted by p-value
 barplot(go_blood, showCategory = 15) + ggtitle("GO terms - Blood Case vs Blood Control")
 
-#web plot: show relationship between go termes
+#web plot: show relationship between go terms
 goplot(go_blood, showCategory = 10) + ggtitle("GO terms - Blood Case vs Blood Control")
 
 #dot plot: gene ratio
 dotplot(go_blood) + ggtitle("GO terms - Blood Case vs Blood Control")
 
-#############################
+
+# # # # # # # # # # # # # # #
 # Lung Case vs Lung Control #
-#############################
+# # # # # # # # # # # # # # #
 
 go_lung <- enrichGO(gene = row.names(DE_lung_with_gene_names), universe = names(dds), 
                      OrgDb = org.Mm.eg.db, ont= "BP", keyType = "ENSEMBL")
 
 head(go_lung)
 
+
 #barplot: count by go terms, sorted by p-value
 barplot(go_lung, showCategory = 15) + ggtitle("GO terms - Lung Case vs Lung Control")
 
-#web plot: show relationship between go termes
+#web plot: show relationship between go terms
 goplot(go_lung, showCategory = 10) + ggtitle("GO terms - Lung Case vs Lung Control")
 
 #dotplot: gene ratio
