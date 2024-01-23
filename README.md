2b70834c3538eca98b3a18001ba32fb63536bea7
Oriane Kopp <okopp@login8.hpc.binf.unibe.ch>
2024-01-23 13:35:33 +0100

    update readme and other stuff

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
 # DE Toxoplasma - rna-seq course
-In this repository, you will find all the code to reproduce my analysis based on the reads from *insert paper's name*
+In this repository, you will find all the code to reproduce my analysis based on the reads from Singhania *et al.*
 ## Steps
 ### Qualtiy check
-Run script 01 and 01_1
+First use fastQC: 01_QC.sh, then use MultiQC to have all your fastqc in one file: 01_1MultiQC.sh.
+There is also a code to have the number of reads: 02_nbreads.sh it's not needed since we have this information in the fastqc files.
+### Creating the index
+First, you will need to download the reference genome from ensembl. Then run 03_index.sh
+
+### Mapping the reads against the reference genome
+To do that, run 04_mapping.sh, 05_sam_to_bam.sh, 06_sorting.sh and 07_indexing.sh
+Then you will have a .bam and a .bam.bai file for each of your reads.
+
+### Counting the number of reads per genes
+You will need to download the annotation corresponding to the reference genome you download before. Then run 08_reads_per_genes.sh and 09_count_table_for_DESeq2.sh to get the count table that you will use for the next step. Download it locally.
+
+### Differential expression analysis 
+To run the differential expression analysis, you will need another data table looking like that:
+Sample	Group
+SRR7821918	Lung_WT_Case
+SRR7821919	Lung_WT_Case
+SRR7821920	Lung_WT_Case
+SRR7821921	Lung_WT_Case
+SRR7821922	Lung_WT_Case
+SRR7821937	Lung_WT_Control
+SRR7821938	Lung_WT_Control
+SRR7821939	Lung_WT_Control
+SRR7821949	Blood_WT_Case
+SRR7821950	Blood_WT_Case
+SRR7821951	Blood_WT_Case
+SRR7821952	Blood_WT_Case
+SRR7821953	Blood_WT_Case
+SRR7821968	Blood_WT_Control
+SRR7821969	Blood_WT_Control
+SRR7821970	Blood_WT_Control
+
+Then you can run the R scrip [insert official name] and you will get everything.
+
+## On this repository, you will also find:
+### all the fastqc and multiqc reports
+### a recap from the quality of the mapping
--- exploratorydataanalysis.R
+++ exploratorydataanalysis.R
@@ -1,6 +1,12 @@
+##########################################################################################################
+#---------------------------------------------------------------------------------------------------------
+##########################################################################################################
 #Exploratory Data Analysis
 
+#set directory
 setwd(dir = "C:/Users/Orian/OneDrive/Documents/Master")
+
+#install packages
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install('EnhancedVolcano')
@@ -8,17 +14,15 @@ BiocManager::install("DESeq2")
 BiocManager::install("biomaRt")
 BiocManager::install("clusterProfiler")
 BiocManager::install("org.Mm.eg.db")
-install.packages("pheatmap")
-
 
 
 #load library
-library(EnhancedVolcano)
 library(DESeq2)
 library(biomaRt)
-library(pheatmap)
-library(clusterProfiler)
 library(org.Mm.eg.db)
+library(EnhancedVolcano)
+library(clusterProfiler)
+
 
 #import the count table and the column table (with information about the samples)
 count_table <- read.table("final_count_table.txt", header = T, row.names = 1, sep = "\t")
@@ -26,6 +30,8 @@ col_table <- read.table("data_table.txt", header = T, row.names = 1, sep = "\t")
 
 #create the DESeqDataSet object
 dds <- DESeqDataSetFromMatrix(countData = count_table, colData = col_table, design = ~ Group)
+
+#perform the DE
 dds <- DESeq(dds)
 
 #PCA to vizualise the data
@@ -36,14 +42,18 @@ plotPCA(vsd, intgroup=c("Group"))
 #---------------------------------------------------------------------------------------------------------
 ##########################################################################################################
 #Differential Expression Analysis
-#blood
+
+###############################
+# Blood Case vs Blood Control #
+###############################
+
 results_blood <- results(dds, contrast=c("Group","Blood_WT_Case","Blood_WT_Control"))
 DE_blood <- results_blood[which(results_blood$padj < 0.05),]
 summary(DE_blood)
 
 
-#add genes name
-ensembl <- useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+#add genes name from ensembl, need to specify the version of the reference used previously (in our case, version 110)
+ensembl <- useEnsembl(biomart = 'genes', dataset = 'mmusculus_gene_ensembl', version = 110)
 ensembl_gene_ids_b <- rownames(DE_blood)
 gene_info_b <- getBM(attributes = c("ensembl_gene_id", "external_gene_name"),
                    filters = "ensembl_gene_id",
@@ -52,35 +62,34 @@ gene_info_b <- getBM(attributes = c("ensembl_gene_id", "external_gene_name"),
 
 DE_blood_with_gene_names <- cbind(gene_info_b, DE_blood)
 
+#sort DE genes by pvalue to get the ten most DE genes
 sort_blood <- DE_blood_with_gene_names[order(DE_blood_with_gene_names$padj),]
 sort_blood[1:10,]
 
 
-#volcano plot
+#volcano plot to visualize DE genes
 EnhancedVolcano(DE_blood_with_gene_names,
                 lab = DE_blood_with_gene_names$external_gene_name,
                 x = 'log2FoldChange',
                 y = 'pvalue',
                 col = c("pink3", "olivedrab", "cyan3", "coral1"),
-                title = "DE genes - blood")
+                title = "DE genes - Blood Case vs Blood Control")
 
-#significant genes
+#explore expression of genes from the article
 Rsad2 <-DE_blood["ENSMUSG00000020641",]
 Rsad2$log2FoldChange > 0 #False --> underexpression
-oas1a <-DE_blood["ENSMUSG00000052776",]
-oas1a$log2FoldChange > 0 #true -- >overexpression
-
-
 Rsad2 <- plotCounts(dds, "ENSMUSG00000020641", intgroup = c("Group"), returnData = TRUE)
 boxplot(count ~ Group , data=Rsad2, main = "Expression of Rsad2")
 
+oas1a <-DE_blood["ENSMUSG00000052776",]
+oas1a$log2FoldChange > 0 #true -- >overexpression
 oas1a <- plotCounts(dds, "ENSMUSG00000052776", intgroup = c("Group"), returnData = TRUE)
 boxplot(count ~ Group , data=oas1a, main = "Expression of oas1a")
 
-########################################################################################################
-#-------------------------------------------------------------------------------------------------------
-########################################################################################################
-#lung
+#############################
+# Lung Case vs Lung Control #
+#############################
+
 results_lung <- results(dds, contrast=c("Group","Lung_WT_Case","Lung_WT_Control"))
 DE_lung <- results_lung[which(results_lung$padj < 0.05),]
 summary(DE_lung)
@@ -105,16 +114,16 @@ EnhancedVolcano(DE_lung_with_gene_names,
                 x = 'log2FoldChange',
                 y = 'pvalue',
                 col = c("pink3", "olivedrab", "cyan3", "coral1"),
-                title = "DE genes - lung")
+                title = "DE genes -  Lung Case vs Lung Control")
 
+#explore expression of genes from the article
 irf7 <-DE_lung["ENSMUSG00000025498",]
 irf7$log2FoldChange > 0 #True --> overexpression
-fcgr1 <-DE_lung["ENSMUSG00000015947",]
-fcgr1$log2FoldChange > 0 #True -- >overexpression
-
 irf7 <- plotCounts(dds, "ENSMUSG00000025498", intgroup = c("Group"), returnData = TRUE)
 boxplot(count ~ Group , data=irf7, main = "Expression of irf7")
 
+fcgr1 <-DE_lung["ENSMUSG00000015947",]
+fcgr1$log2FoldChange > 0 #True -- >overexpression
 fcgr1 <- plotCounts(dds, "ENSMUSG00000015947", intgroup = c("Group"), returnData = TRUE)
 boxplot(count ~ Group , data=fcgr1, main = "Expression of fcgr1")
 
@@ -122,20 +131,40 @@ boxplot(count ~ Group , data=fcgr1, main = "Expression of fcgr1")
 #---------------------------------------------------------------------------------------------------------
 ##########################################################################################################
 #Overexpression Analysis 
-#blood
+
+###############################
+# Blood Case vs Blood Control #
+###############################
+
 go_blood <- enrichGO(gene = row.names(DE_blood_with_gene_names), universe = names(dds), 
                      OrgDb = org.Mm.eg.db, ont= "BP", keyType = "ENSEMBL")
 
 head(go_blood)
-barplot(go_blood, showCategory = 15) + ggtitle("GO terms - blood")
-goplot(go_blood, showCategory = 10)
-dotplot(go_blood)
 
-#lung
+#barplot: count by go terms, sorted by p-value
+barplot(go_blood, showCategory = 15) + ggtitle("GO terms - Blood Case vs Blood Control")
+
+#web plot: show relationship between go termes
+goplot(go_blood, showCategory = 10) + ggtitle("GO terms - Blood Case vs Blood Control")
+
+#dot plot: gene ratio
+dotplot(go_blood) + ggtitle("GO terms - Blood Case vs Blood Control")
+
+#############################
+# Lung Case vs Lung Control #
+#############################
+
 go_lung <- enrichGO(gene = row.names(DE_lung_with_gene_names), universe = names(dds), 
                      OrgDb = org.Mm.eg.db, ont= "BP", keyType = "ENSEMBL")
 
 head(go_lung)
-barplot(go_lung, showCategory = 15) + ggtitle("GO terms - lung")
-goplot(go_lung, showCategory = 10)
-dotplot(go_lung)
+
+#barplot: count by go terms, sorted by p-value
+barplot(go_lung, showCategory = 15) + ggtitle("GO terms - Lung Case vs Lung Control")
+
+#web plot: show relationship between go termes
+goplot(go_lung, showCategory = 10) + ggtitle("GO terms - Lung Case vs Lung Control")
+
+#dotplot: gene ratio
+dotplot(go_lung) + ggtitle("GO terms - Lung Case vs Lung Control")
+
